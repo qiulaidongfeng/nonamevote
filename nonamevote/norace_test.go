3504f8d09f9bb227c260b7fd5c35201a42953370
qiulaidongfeng <2645477756@qq.com>
2025-03-09 02:04:03 +0800

    修复基准测试失败

--- nonamevote/norace_test.go
+++ nonamevote/norace_test.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"gitee.com/qiulaidongfeng/nonamevote/internal/account"
+	"gitee.com/qiulaidongfeng/nonamevote/internal/safe"
 
 	"github.com/pquerna/otp"
 	"github.com/pquerna/otp/totp"
@@ -20,7 +21,7 @@ func BenchmarkLogin(b *testing.B) {
 
 	benchmark(b, req, func(req *http.Request) {
 		u := account.UserDb.Find("k")
-		k, _ := otp.NewKeyFromURL(u.TotpURL)
+		k, _ := otp.NewKeyFromURL(safe.Decrypt(u.TotpURL))
 		code, _ := totp.GenerateCodeCustom(k.Secret(), time.Now(), totp.ValidateOpts{})
 
 		req.PostForm = url.Values{
